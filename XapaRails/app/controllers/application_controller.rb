b989381233ca53ff66a827eb78149adc81cd6995
Adesegun Adedeji <adesegunadedeji@gmail.com>
2019-10-22 21:44:09 -0400

    Added  React App

--- XapaRails/app/controllers/application_controller.rb
+++ XapaRails/app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+    skip_before_action :verify_authenticity_token #set of rules when we communicate with route CSRF Token.
 end
--- XapaRails/config/initializers/session_store.rb
+++ XapaRails/config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_authentication_xapa",domain: "whatvertheapiis.heroku.com"
+if Rails.env = "production"
+    Rails.application.config.session_store :cookie_store, key: "_authentication_xapa",domain: "whatvertheapiis.heroku.com"
+else
+    Rails.application.config.session_store :cookie_store, key: "_authentication_xapa"
+end
